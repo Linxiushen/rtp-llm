8360027a3bbd25567c23f9c0bb68edfdecf6836d
hengcang.wyd <hengcang.wyd@alibaba-inc.com>
2026-08-11 10:58:24 +0800

    fix(loader): adapt new models to current main APIs

--- rtp_llm/test/kv_cache_specs_test.py
+++ rtp_llm/test/kv_cache_specs_test.py
@@ -149,6 +149,7 @@ class HybridKVCacheSpecTest(TestCase):
             {
                 "vision_start_token_id": 1,
                 "vision_end_token_id": 2,
+                "vision_config": {},
                 "text_config": {
                     "intermediate_size": 256,
                     "num_attention_heads": 2,
@@ -189,6 +190,7 @@ class HybridKVCacheSpecTest(TestCase):
                 {
                     "vision_start_token_id": 1,
                     "vision_end_token_id": 2,
+                    "vision_config": {},
                     "text_config": {
                         "intermediate_size": 256,
                         "num_attention_heads": 2,
